fde94b05a84b27c62a28db3a9b619be49f9ed318
Rodrigo Petters <rodrigofmp@gmail.com>
2025-05-20 18:46:41 -0300

    First blood
    Default FirstPersonShooter project from Unreal

---
+++ Source/ProjetoFPS/ProjetoFPS.cpp
@@ -0,0 +1,7 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "ProjetoFPS.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, ProjetoFPS, "ProjetoFPS" );
+ 
---
+++ Source/ProjetoFPS/ProjetoFPSGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "ProjetoFPSGameMode.generated.h"
+
+UCLASS(minimalapi)
+class AProjetoFPSGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	AProjetoFPSGameMode();
+};
+
+
+
